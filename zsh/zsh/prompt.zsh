f0300c1a79f3b19cca494cab5c8dfccc6397b130
aonemd <aonemdsaleh@gmail.com>
2016-09-14 00:40:19 +0200

    Fix shell prompt git status.
    It seems, Git clean directory message 'working directory clean' was
    replaced by 'working tree clean' for some reason.

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,5 +1,5 @@
 function git_status {
-  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]]; then 
+  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
     echo "%{$fg[red]%}"
   else
     echo "%{$fg[green]%}"
@@ -9,7 +9,7 @@ function git_status {
 function git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ $(git_status)\1/"
 }
- 
+
 function hg_status {
   if [[ $( hg status 2> /dev/null ) != "" ]]; then
     echo "%{$fg[red]%}"
@@ -21,5 +21,5 @@ function hg_status {
 function hg_branch {
   hg branch 2> /dev/null | sed -e "s/\(.*\)/ $(hg_status)\1/"
 }
- 
+
 PROMPT='%{$fg[blue]%}%1~$(git_branch)$(hg_branch) %{$reset_color%}➜ '
