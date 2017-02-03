1f9c9e8f6233a2c7c73ca4a22823df8eb5630da5
aonemd <aonemdsaleh@gmail.com>
2017-02-03 23:16:55 +0200

    Use `RFC2822 style` for git commit date

--- zsh/zsh/constants.zsh
+++ zsh/zsh/constants.zsh
@@ -1,1 +1,1 @@
-git_log_normal_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
+git_log_normal_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%aD (%ar)%C(reset)%n%+B'
