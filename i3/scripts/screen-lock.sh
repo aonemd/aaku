49c030260f7c8b2b6ae68bd6581917ebd8198a41
aonemd <aonemdsaleh@gmail.com>
2017-03-15 20:58:19 +0000

    Add i3wm-gaps configuration files

---
+++ i3/scripts/screen-lock.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# i3lock screen lock script
+# requires imagemagick, scrot
+
+lockbg="/tmp/screen.png"
+
+if [ $1 ]; then
+  lockfg="$1"
+else
+  lockfg="$HOME/.config/i3/scripts/lockfg.png"
+fi
+
+scrot -z "$lockbg"
+
+convert "$lockbg" -scale 10% -scale 1000% -blur 0x8 "$lockbg"
+convert "$lockbg" \( "$lockfg" -scale 77% -gravity center \) -composite -matte "$lockbg"
+
+setxkbmap us  # change layout to us
+
+i3lock -u -n -e -i "$lockbg" >> /dev/null
+
+rm "$lockbg"
