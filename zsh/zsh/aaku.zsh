f774d757d035ce4b302c4dd9de987105aa53ab83
aonemd <aonemdsaleh@gmail.com>
2017-03-29 20:31:57 +0000

    Use vi navigation keys in menu completion

--- zsh/zsh/aaku.zsh
+++ zsh/zsh/aaku.zsh
@@ -14,6 +14,11 @@ autoload -U compinit && compinit
 zstyle ':completion:*' menu select
 zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# use the vi navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
 
 # History
 export HISTSIZE=25000
