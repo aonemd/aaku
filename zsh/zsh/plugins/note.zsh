ea5a8835f43e8a9e31c520088d5b2d603c1ad0df
aonemd <aonemdsaleh@gmail.com>
2017-07-16 00:34:36 +0200

    Add `note` zsh plugin to manage taking notes

---
+++ zsh/zsh/plugins/note.zsh
@@ -0,0 +1,22 @@
+note () {
+  local notes_dir="$WK/notes"
+
+  case "$1" in
+    c)
+      cd "$notes_dir"
+      ;;
+    l)
+      ls "$notes_dir"
+      ;;
+    p)
+      pushd "$notes_dir"
+      msg="Regenerated at $(date -u '+%Y-%m-%d %H:%M:%S') UTC"
+      git add .
+      git commit -m "$msg"
+      git push origin master
+      popd
+      ;;
+    *)
+      vim "$notes_dir/$1"
+  esac
+}
