4fa0836e4268bb0043670f74d1e2d91f270957e2
aonemd <aonemdsaleh@gmail.com>
2017-08-12 17:11:15 +0200

    Handle running `ts` inside running tmux session

--- zsh/zsh/plugins/ts.zsh
+++ zsh/zsh/plugins/ts.zsh
@@ -12,17 +12,18 @@ ts () {
   pushd "${WK}/${session_path}"
 
   tmux new-session   -d -s "${session_name}"
-  tmux split-window  -v
-  tmux split-window  -h
-  tmux resize-pane   -D 17
-  tmux select-pane   -t 0
-  tmux send-keys     'vim ' C-m
+
+  tmux send-keys     -t "${session_name}":1 'vim ' C-m
+  tmux split-window  -t "${session_name}":1 -v
+  tmux split-window  -t "${session_name}":1 -h
+  tmux resize-pane   -t "${session_name}":1 -D 17
+  tmux select-pane   -t "${session_name}":1.0
 
   tmux new-window    -t "${session_name}":2 -n 'log'
   tmux new-window    -t "${session_name}":3 -n 'misc'
 
-  tmux select-window -t 1
-  tmux attach        -t "${session_name}"
+  tmux select-window -t "${session_name}":1
+  [ -n "$TMUX" ] && tmux switch -t "${session_name}" || tmux attach -t "${session_name}"
 
   popd
 }
