189dddf7188c85eb2e41a13d1caa7818a53c7cf9
aonemd <aonemdsaleh@gmail.com>
2017-11-26 19:12:55 +0200

    Move towards a flatter directory

--- install.sh
+++ install.sh
@@ -6,53 +6,50 @@ echo "Updating submodules..."
 git submodule update --init --recursive --remote
 
 echo "Linking files..."
-ln -sfn ${BASEDIR}/vim/vim/ $HOME/.vim
+ln -sfn ${BASEDIR}/vim/vim $HOME/.vim
 ln -sfn ${BASEDIR}/vim/vimrc $HOME/.vimrc
 ln -sfn ${BASEDIR}/vim/gvimrc $HOME/.gvimrc
 
-ln -sfn ${BASEDIR}/zsh/zsh/ $HOME/.zsh
+ln -sfn ${BASEDIR}/zsh/zsh $HOME/.zsh
 ln -sfn ${BASEDIR}/zsh/zprofile $HOME/.zprofile
 ln -sfn ${BASEDIR}/zsh/zshenv $HOME/.zshenv
 ln -sfn ${BASEDIR}/zsh/zshrc $HOME/.zshrc
 
 ln -sfn ${BASEDIR}/tmux/tmux.conf $HOME/.tmux.conf
-
-ln -sfn ${BASEDIR}/app-config/gitconfig $HOME/.gitconfig
-ln -sfn ${BASEDIR}/app-config/irbrc $HOME/.irbrc
-ln -sfn ${BASEDIR}/app-config/agignore $HOME/.agignore
-ln -sfn ${BASEDIR}/app-config/fehbg $HOME/.fehbg
-
-[ -d $HOME/.config/termite/ ] || mkdir -p $HOME/.config/termite
-ln -sfnr ${BASEDIR}/app-config/termite $HOME/.config/termite/config
-
-[ -d $HOME/.config/rofi/ ] || mkdir -p $HOME/.config/rofi
-ln -sfnr ${BASEDIR}/app-config/rofi $HOME/.config/rofi/config
-
-[ -d $HOME/.config/dunst/ ] || mkdir -p $HOME/.config/dunst
-ln -sfnr ${BASEDIR}/app-config/dunstrc $HOME/.config/dunst/dunstrc
-
-[ -d $HOME/.config/compton/ ] || mkdir -p $HOME/.config/compton
-ln -sfnr ${BASEDIR}/app-config/compton.conf $HOME/.config/compton/compton.conf
+ln -sfn ${BASEDIR}/git/gitconfig $HOME/.gitconfig
+ln -sfn ${BASEDIR}/ruby/irbrc $HOME/.irbrc
+ln -sfn ${BASEDIR}/ag/agignore $HOME/.agignore
 
 [ -d $HOME/.config/cmus/ ] || mkdir -p $HOME/.config/cmus
-ln -sfnr ${BASEDIR}/app-config/cmus-theme $HOME/.config/cmus/aaku.theme
+ln -sfn ${BASEDIR}/cmus/aaku.theme $HOME/.config/cmus/aaku.theme
 
 [ -d $HOME/.irssi ] || mkdir -p $HOME/.irssi
-ln -sfnr ${BASEDIR}/app-config/irssi/config $HOME/.irssi/config
-ln -sfnr ${BASEDIR}/app-config/irssi/weed.theme $HOME/.irssi/weed.theme
-ln -sfnr ${BASEDIR}/app-config/irssi/scripts $HOME/.irssi/scripts
+ln -sfn ${BASEDIR}/irssi/config $HOME/.irssi/config
+ln -sfn ${BASEDIR}/irssi/weed.theme $HOME/.irssi/weed.theme
+ln -sfn ${BASEDIR}/irssi/scripts $HOME/.irssi/scripts
 
-ln -sfnr ${BASEDIR}/X11/xinitrc $HOME/.xinitrc
-ln -sfnr ${BASEDIR}/X11/Xresources $HOME/.Xresources
+ln -sfn ${BASEDIR}/X11/xinitrc $HOME/.xinitrc
+ln -sfn ${BASEDIR}/X11/Xresources $HOME/.Xresources
 
-[ -d $HOME/.config/i3/ ] || mkdir -p $HOME/.config/i3
-ln -sfnr ${BASEDIR}/i3/config $HOME/.config/i3/config
-ln -sfnr ${BASEDIR}/i3/i3status.conf $HOME/.config/i3/i3status.conf
-ln -sfnr ${BASEDIR}/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf
-ln -sfnr ${BASEDIR}/i3/scripts $HOME/.config/i3/scripts
-ln -sfnr ${BASEDIR}/i3/blocklets $HOME/.config/i3/blocklets
+ln -sfn ${BASEDIR}/i3 $HOME/.config/i3
 
-ln -sfnr ${BASEDIR}/fontconfig/ $HOME/.config/fontconfig
+ln -sfn ${BASEDIR}/i3blocks $HOME/.config/i3blocks
+
+ln -sfn ${BASEDIR}/bin/fehbg $HOME/.fehbg
+
+[ -d $HOME/.config/termite/ ] || mkdir -p $HOME/.config/termite
+ln -sfn ${BASEDIR}/termite/config $HOME/.config/termite/config
+
+[ -d $HOME/.config/rofi/ ] || mkdir -p $HOME/.config/rofi
+ln -sfn ${BASEDIR}/rofi/config $HOME/.config/rofi/config
+
+[ -d $HOME/.config/dunst/ ] || mkdir -p $HOME/.config/dunst
+ln -sfn ${BASEDIR}/dunst/dunstrc $HOME/.config/dunst/dunstrc
+
+[ -d $HOME/.config/compton/ ] || mkdir -p $HOME/.config/compton
+ln -sfn ${BASEDIR}/compton/compton.conf $HOME/.config/compton/compton.conf
+
+ln -sfn ${BASEDIR}/fontconfig $HOME/.config/fontconfig
 
 echo "Installing Vim plugins ..."
 echo | vim - +PlugInstall +qall
