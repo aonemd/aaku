25c72bdbfd3928c6685288be5af9878bc801b5f4
aonemd <aonemdsaleh@gmail.com>
2017-12-21 08:15:21 +0200

    Refactor Zsh prompt

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,17 +1,20 @@
-function git_status {
-  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
-    echo "%{$fg[red]%}"
-  else
-    echo "%{$fg[green]%}"
-  fi
+PROMPT_INSERT_SYMBOL="›"
+
+function prompt_current_dir {
+  echo -n "%{$fg[blue]%}%1~"
+}
+
+function prompt_symbol {
+  local current_symbol="$PROMPT_INSERT_SYMBOL"
+  echo -n "%{$fg[white]%}$current_symbol"
 }
 
 function git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ $(git_status)\1/"
 }
 
-function hg_status {
-  if [[ $( hg status 2> /dev/null ) != "" ]]; then
+function git_status {
+  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
     echo "%{$fg[red]%}"
   else
     echo "%{$fg[green]%}"
@@ -22,4 +25,12 @@ function hg_branch {
   hg branch 2> /dev/null | sed -e "s/\(.*\)/ $(hg_status)\1/"
 }
 
-PROMPT='%{$fg[blue]%}%1~$(git_branch)$(hg_branch) %{$fg_bold[white]%}➜ %{$reset_color%}'
+function hg_status {
+  if [[ $( hg status 2> /dev/null ) != "" ]]; then
+    echo "%{$fg[red]%}"
+  else
+    echo "%{$fg[green]%}"
+  fi
+}
+
+PROMPT='$(prompt_current_dir)$(git_branch)$(hg_branch) $(prompt_symbol) %{$reset_color%}'
