3ebf76d6d62cbbf74c232bcafbb06ad3ad88a916
aonemd <aonemdsaleh@gmail.com>
2020-09-18 22:12:42 +0200

    Add kp.zsh for finding and killing processes

---
+++ zsh/zsh/plugins/kp.zsh
@@ -0,0 +1,9 @@
+kp() {
+  local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+    kp
+  fi
+}
