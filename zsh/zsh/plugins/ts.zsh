c61d16166274a9dabe41225d3e88141f1c97a9c0
aonemd <aonemdsaleh@gmail.com>
2020-10-19 13:18:11 +0200

    Update `ts.zsh`; find Tmux session by exact name match

--- zsh/zsh/plugins/ts.zsh
+++ zsh/zsh/plugins/ts.zsh
@@ -4,7 +4,7 @@ ts() {
   local session_path="${1}"
   local session_name="${1##*/}"
 
-  if tmux has-session -t "${session_name}" 2> /dev/null; then
+  if tmux has-session -t="${session_name}" 2> /dev/null; then
     tmux attach -t "${session_name}"
     return
   fi
@@ -13,17 +13,17 @@ ts() {
 
   tmux new-session   -d -s "${session_name}"
 
-  tmux send-keys     -t "${session_name}":1 'vim ' C-m
+  tmux send-keys     -t="${session_name}":1 'vim ' C-m
   sleep 0.5
-  tmux split-window  -t "${session_name}":1 -v
-  tmux resize-pane   -t "${session_name}":1 -D 15
-  tmux select-pane   -t "${session_name}":1.1
+  tmux split-window  -t="${session_name}":1 -v
+  tmux resize-pane   -t="${session_name}":1 -D 15
+  tmux select-pane   -t="${session_name}":1.1
 
-  tmux new-window    -t "${session_name}":2 -n 'log'
-  tmux new-window    -t "${session_name}":3 -n 'misc'
+  tmux new-window    -t="${session_name}":2 -n 'log'
+  tmux new-window    -t="${session_name}":3 -n 'misc'
 
-  tmux select-window -t "${session_name}":1
-  [ -n "$TMUX" ] && tmux switch -t "${session_name}" || tmux attach -t "${session_name}"
+  tmux select-window -t="${session_name}":1
+  [ -n "$TMUX" ] && tmux switch -t="${session_name}" || tmux attach -t="${session_name}"
 
   popd > /dev/null
 }
