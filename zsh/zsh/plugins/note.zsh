6cb95952656aae3b502c98343c23feee47554895
aonemd <aonemdsaleh@gmail.com>
2021-01-29 21:50:12 +0200

    Add git syncing abilities to note.zsh

--- zsh/zsh/plugins/note.zsh
+++ zsh/zsh/plugins/note.zsh
@@ -11,6 +11,19 @@ note() {
     t)
       tree --noreport "$notes_dir"
       ;;
+    gp)
+      pushd "$notes_dir"
+      msg="Regenerated at $(date -u '+%Y-%m-%d %H:%M:%S') UTC"
+      git add .
+      git commit -m "$msg"
+      git push origin master
+      popd
+      ;;
+    gpu)
+      pushd "$notes_dir"
+      git pull origin master
+      popd
+      ;;
     *)
       pushd "$notes_dir"
       if [ $# -eq 0 ]; then
