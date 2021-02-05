df571eecff5506dce99300e32b865b4135d852ec
aonemd <aonemdsaleh@gmail.com>
2021-02-05 20:31:59 +0200

    Move variables to `variables.zsh`

---
+++ zsh/zsh/variables.zsh
@@ -0,0 +1,10 @@
+export MEDIA_PLAYER='cmus'
+export BATTERY_NUMBER=0
+export ETHERNET_INTERFACE='enp0s3'
+export WIFI_INTERFACE='wlp4s0'
+export VOLUME_DEVICE='master'
+
+# nnn
+export NNN_COLORS='4213'
+export NNN_BMS='w:~/Workspace;d:~/Documents;v:~/Videos;D:~/Downloads;n:~/Dropbox/notes'
+export NNN_PLUG='l:-launch;'
