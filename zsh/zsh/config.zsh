624b21d199cfc4515ec0393d0d43d7931cbc1919
aonemd <aonemdsaleh@gmail.com>
2021-02-05 21:39:10 +0200

    Add Ubuntu history autocomplete config that works

--- zsh/zsh/config.zsh
+++ zsh/zsh/config.zsh
@@ -41,6 +41,8 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
+# bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+# bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 
 # Vim Mode
 bindkey -v
