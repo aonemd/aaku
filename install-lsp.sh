cba64bf4eac3afc68227c919b52cc55b0169edeb
aonemd <aonemdsaleh@gmail.com>
2021-02-18 19:31:30 +0200

    Add `typescript` to install-lsp script

--- install-lsp.sh
+++ install-lsp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 GO111MODULE=on go get golang.org/x/tools/gopls@latest
-npm install -g typescript-language-server
+npm install -g typescript typescript-language-server
 pacman -S clang
 gem install solargraph
