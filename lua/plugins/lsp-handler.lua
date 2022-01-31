b581cbf911612feb121465113e37ba653ecee268
aonemd <asalehmo7@gmail.com>
2022-01-31 21:15:03 +0100

    Disable LSP signs in signcolumn
    They are annoying and degenerate!

--- lua/plugins/lsp-handler.lua
+++ lua/plugins/lsp-handler.lua
@@ -1,25 +1,9 @@
 local M = {}
 
--- TODO: backfill this to template
 M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
-
   local config = {
-    -- disable virtual text
-    virtual_text = false,
-    -- show signs
-    signs = {
-      active = signs,
-    },
+    virtual_text = false,   -- disable text that appears beside code
+    signs = false,
     update_in_insert = true,
     underline = true,
     severity_sort = true,
