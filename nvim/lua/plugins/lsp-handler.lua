6250b8be7e5844588d7c840c948ac06f0116c896
aonemd <asalehmo7@gmail.com>
2022-02-19 23:09:32 +0100

    nvim: Cleanup LSP config

--- nvim/lua/plugins/lsp-handler.lua
+++ nvim/lua/plugins/lsp-handler.lua
@@ -68,6 +68,7 @@ M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
   end
+
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
