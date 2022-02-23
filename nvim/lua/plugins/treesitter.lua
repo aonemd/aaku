89226333f9394f91c84226faffb9428f507bd5cc
aonemd <asalehmo7@gmail.com>
2022-02-23 21:12:21 +0100

    nvim: Add treesitter scope selection keymaps

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -32,10 +32,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
+      init_selection = 'gtt',
+      node_incremental = 'gtn',
+      scope_incremental = 'gtc',
+      node_decremental = 'gtm',
     },
   },
   indent = {
