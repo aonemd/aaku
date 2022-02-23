e3a930c85912bbd8e326d75c82df587e4d24e646
aonemd <asalehmo7@gmail.com>
2022-02-23 21:03:13 +0100

    nvim: Add gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,19 +10,12 @@ require('gitsigns').setup {
     map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
 
     -- -- Actions
-    -- map('n', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
-    -- map('v', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
-    -- map('n', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
-    -- map('v', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
+    map('n', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
+    map('v', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
+    map('n', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
+    map('v', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
 
-    -- map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-    -- map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-    -- map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-    -- map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-
-    map('n', '<leader>gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-    -- map('n', '<leader>gB', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-    -- map('n', '<leader>gd', '<cmd>Gitsigns toggle_deleted<CR>')
+    map('n', '<leader>cb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
 
     -- Text object
     map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
