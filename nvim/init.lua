23de2b6e05f53dc61ff7ad2101f0cf851f365369
aonemd <asalehmo7@gmail.com>
2022-05-17 08:21:12 +0200

    Add terminal toggle function and maps
    Thanks: https://www.reddit.com/r/vim/comments/8n5bzs/using_neovim_is_there_a_way_to_display_a_terminal/dzt3fix/?utm_source=reddit&utm_medium=web2x&context=3

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,3 +69,37 @@ vim.cmd [[
     autocmd FileType markdown set wrap linebreak textwidth=80
   augroup END
 ]]
+
+vim.cmd [[
+" Terminal Function
+let g:term_buf = 0
+let g:term_win = 0
+function! TermToggle(height)
+    if win_gotoid(g:term_win)
+        hide
+    else
+        botright new
+        exec "resize " . a:height
+        try
+            exec "buffer " . g:term_buf
+        catch
+            call termopen($SHELL, {"detach": 0})
+            let g:term_buf = bufnr("")
+            set nonumber
+            set norelativenumber
+            set signcolumn=no
+        endtry
+        startinsert!
+        let g:term_win = win_getid()
+    endif
+endfunction
+
+" Toggle terminal on/off (neovim)
+nnoremap <C-t> :call TermToggle(12)<CR>
+inoremap <C-t> <Esc>:call TermToggle(12)<CR>
+tnoremap <C-t> <C-\><C-n>:call TermToggle(12)<CR>
+
+" Terminal go back to normal mode
+tnoremap <Esc> <C-\><C-n>
+tnoremap :qq <C-\><C-n>:q!<CR>
+]]
