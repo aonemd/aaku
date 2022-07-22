a17655b32296482a5cb6543bf9f88c0acaba5bf2
aonemd <asalehmo7@gmail.com>
2022-07-22 20:19:27 +0200

    nvim: treesitter settings, disable `ruby`; it's a turle

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,13 @@
-
---[[ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-  return
-end
-
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-} ]]
-
--- Treesitter
 -- Parsers must be installed manually via :TSInstall.
 require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "scheme", "rust", "c", "typescript", "javascript", "ruby" },
+
   highlight = {
     enable = true, -- false will disable the whole extension.
+
+    additional_vim_regex_highlighting = false,
+
+    disable = { "ruby" },
   },
   incremental_selection = {
     enable = true,
