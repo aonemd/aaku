5c9710c84d6f2589cdcfce70d679f7d4f1f6d7ad
aonemd <asalehmo7@gmail.com>
2022-07-24 22:16:56 +0200

    nvim: move terminal config to terminal lua module

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,8 +53,8 @@ require("keybindings")
 require("plugins")
 vim.cmd('source $HOME/.config/nvim/funks.vim')
 
+require("terminal")
 require("statusline")
-
 require("funks")
 SetTheme()
 
@@ -68,40 +68,6 @@ vim.cmd [[
     autocmd!
     autocmd FileType markdown set wrap linebreak
   augroup END
-]]
-
-vim.cmd [[
-" Terminal Function
-let g:term_buf = 0
-let g:term_win = 0
-function! TermToggle(height)
-    if win_gotoid(g:term_win)
-        hide
-    else
-        botright new
-        exec "resize " . a:height
-        try
-            exec "buffer " . g:term_buf
-        catch
-            call termopen($SHELL, {"detach": 0})
-            let g:term_buf = bufnr("")
-            set nonumber
-            set norelativenumber
-            set signcolumn=no
-        endtry
-        startinsert!
-        let g:term_win = win_getid()
-    endif
-endfunction
-
-" Toggle terminal on/off (neovim)
-nnoremap <C-t> :call TermToggle(12)<CR>
-inoremap <C-t> <Esc>:call TermToggle(12)<CR>
-tnoremap <C-t> <C-\><C-n>:call TermToggle(12)<CR>
-
-" Terminal go back to normal mode
-tnoremap <Esc> <C-\><C-n>
-tnoremap :qq <C-\><C-n>:q!<CR>
 ]]
 
 require("hotpot")
---
+++ nvim/lua/terminal.lua
@@ -0,0 +1,43 @@
+vim.cmd [[
+" Terminal Function
+let g:term_buf = 0
+let g:term_win = 0
+function! TermToggle(height)
+    if win_gotoid(g:term_win)
+        hide
+    else
+        botright new
+        exec "resize " . a:height
+        try
+            exec "buffer " . g:term_buf
+        catch
+            call termopen($SHELL, {"detach": 0})
+            let g:term_buf = bufnr("")
+            set nonumber
+            set norelativenumber
+            set signcolumn=no
+        endtry
+        startinsert!
+        let g:term_win = win_getid()
+    endif
+endfunction
+
+" Toggle terminal on/off (neovim)
+nnoremap <C-t> :call TermToggle(12)<CR>
+inoremap <C-t> <Esc>:call TermToggle(12)<CR>
+tnoremap <C-t> <C-\><C-n>:call TermToggle(12)<CR>
+
+" Terminal go back to normal mode
+tnoremap <Esc> <C-\><C-n>
+tnoremap :qq <C-\><C-n>:q!<CR>
+
+" Terminal navigation
+tnoremap <C-h> <C-\><C-n><C-w>h
+tnoremap <C-j> <C-\><C-n><C-w>j
+tnoremap <C-k> <C-\><C-n><C-w>k
+tnoremap <C-l> <C-\><C-n><C-w>l
+]]
+
+
+vim.g.neoterm_size = tostring(0.3 * vim.o.columns)
+vim.g.neoterm_default_mod = 'botright vertical'
