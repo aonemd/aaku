ff72659791c412cf13e276b25023cdc1537766db
aonemd <asalehmo7@gmail.com>
2022-08-30 21:21:59 +0200

    nvim: cmp select with enter

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -17,7 +17,7 @@ local mappings = cmp.mapping.preset.insert({
   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
   ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-  ["<CR>"]      = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false, },
+  ['<CR>']      = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   ['<C-Space>'] = cmp.mapping.complete(),
   ["<C-y>"]     = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
   ["<C-e>"]     = cmp.mapping {
