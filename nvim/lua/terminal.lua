f26c01be7f3e659630075b0fccf4204dadd0d447
aonemd <asalehmo7@gmail.com>
2022-08-30 20:32:25 +0200

    nvim: these temrinal maps break window navigation

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -30,12 +30,6 @@ tnoremap <C-t> <C-\><C-n>:call TermToggle(12)<CR>
 " Terminal go back to normal mode
 tnoremap <Esc> <C-\><C-n>
 tnoremap :qq <C-\><C-n>:q!<CR>
-
-" Terminal navigation
-tnoremap <C-h> <C-\><C-n><C-w>h
-tnoremap <C-j> <C-\><C-n><C-w>j
-tnoremap <C-k> <C-\><C-n><C-w>k
-tnoremap <C-l> <C-\><C-n><C-w>l
 ]]
 
 
