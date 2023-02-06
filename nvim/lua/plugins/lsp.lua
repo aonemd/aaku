aeeb0db6d8f63e077be8b8e57b831a0299350f40
aonemd <asalehmo7@gmail.com>
2023-02-06 18:38:11 +0100

    nvim: Add LSP floating window borders

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -37,17 +37,27 @@ vim.diagnostic.config({
   signs            = true,
   underline        = true,
   update_in_insert = false,
-  severity_sort    = false,
+  severity_sort    = true,
 
   float = {
     focusable = false,
     style     = "minimal",
-    border    = "rounded",
-    source    = "if_many",
-    header    = "",
-    prefix    = "",
+    border    = 'rounded',
+    source    = 'always',
+    header    = '',
+    prefix    = '',
   },
 })
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
