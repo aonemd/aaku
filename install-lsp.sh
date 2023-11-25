e7fe9cce83637049434504ea90b200b27f711661
aonemd <asalehmo7@gmail.com>
2023-11-25 20:32:35 +0100

    bin: Delete install-lsp.sh

--- install-lsp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-pacman -S clang
-npm install -g typescript typescript-language-server
-npm install -g vim-language-server
-GO111MODULE=on go get golang.org/x/tools/gopls@latest
-gem install solargraph
