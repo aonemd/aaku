734d5585d3b9bd5fc0810f496c7a0bada1b9ec07
aonemd <asalehmo7@gmail.com>
2023-11-25 21:28:12 +0100

    nvim: `set nvim-web-devicons` as a dep for nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
