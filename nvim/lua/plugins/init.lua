0d53e8008c4e1c1b056b3b32947cc3cb9c68e6ad
aonemd <asalehmo7@gmail.com>
2024-01-06 19:47:28 +0100

    nvim: Add which-key for now

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,6 +21,19 @@ require('lazy').setup({
   { import = 'plugins.gitsigns' },
   'rktjmp/hotpot.nvim',
   {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
     'nvim-tree/nvim-web-devicons',
     config = function()
       require('nvim-web-devicons').setup()
