a1005be8be3e5c11565f57a1e7f5c8d0825a2b30
aonemd <asalehmo7@gmail.com>
2024-02-06 13:25:42 +0100

    zsh: Add `ffw` plugin for finding&opening projects

---
+++ zsh/zsh/plugins/ffw.zsh
@@ -0,0 +1,15 @@
+# Based on: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+
+ffw() {
+  if [[ $# -eq 1 ]]; then
+    selected=$1
+  else
+    selected=$(find ~/Workspace/ -mindepth 1 -maxdepth 1 -type d | fzf)
+  fi
+
+  if [[ -z $selected ]]; then
+    exit 0
+  fi
+
+  tx $selected
+}
