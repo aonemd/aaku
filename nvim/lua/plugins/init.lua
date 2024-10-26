853f91ae57242eb9410d591ec8c9a1eb7e5252e5
aonemd <asalehmo7@gmail.com>
2024-10-26 18:20:33 +0200

    nvim: Add eyeliner :eye:

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -116,6 +116,31 @@ require('lazy').setup({
     end,
   },
   {
+    'jinh0/eyeliner.nvim',
+    config = function()
+      require 'eyeliner'.setup {
+        -- show highlights only after keypress
+        highlight_on_key = true,
+        -- dim all other characters if set to true (recommended!)
+        dim = false,
+        -- set the maximum number of characters eyeliner.nvim will check from
+        -- your current cursor position; this is useful if you are dealing with
+        -- large files: see https://github.com/jinh0/eyeliner.nvim/issues/41
+        max_length = 9999,
+        -- filetypes for which eyeliner should be disabled;
+        -- e.g., to disable on help files:
+        -- disabled_filetypes = {"help"}
+        disabled_filetypes = {},
+        -- buftypes for which eyeliner should be disabled
+        -- e.g., disabled_buftypes = {"nofile"}
+        disabled_buftypes = {},
+        -- add eyeliner to f/F/t/T keymaps;
+        -- see section on advanced configuration for more information
+        default_keymaps = true,
+      }
+    end
+  },
+  {
     'norcalli/nvim-colorizer.lua',
     config = function()
       require('colorizer').setup()
