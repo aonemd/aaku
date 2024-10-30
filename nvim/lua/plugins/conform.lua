d5c4bfa7df9ee1addea33c609038132dea1b04f4
aonemd <asalehmo7@gmail.com>
2024-10-30 20:19:41 +0100

    nvim: Conform command + yaml formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,24 +1,41 @@
 return {
-  "stevearc/conform.nvim",
+  'stevearc/conform.nvim',
   lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
   config = function()
-    require("conform").setup({
+    require('conform').setup({
       formatters_by_ft = {
-        lua             = { "stylua" },
-        go              = { "goimports", "gofmt" },
-        rust            = { "rustfmt", lsp_format = "fallback" },
-        ruby            = { "rubocop" },
-        javascript      = { "prettier" },
-        typescript      = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
+        lua = { 'stylua' },
+        go = { 'goimports', 'gofmt' },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
+        ruby = { 'rubocop' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        yaml = { 'yamlfmt' },
+        ['*'] = { 'codespell' },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ['_'] = { 'trim_whitespace' },
       },
       format_on_save = {
-        lsp_format = "fallback",
+        lsp_format = 'fallback',
         timeout_ms = 500,
         async = false,
       },
     })
+
+    vim.api.nvim_create_user_command('Format', function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ['end'] = { args.line2, end_line:len() },
+        }
+      end
+      require('conform').format({ async = true, lsp_format = 'fallback', range = range })
+    end, { range = true })
   end,
 }
