908f84944f2bb0a812dfedf6fc7b316088e1dc00
aonemd <asalehmo7@gmail.com>
2024-12-31 10:08:33 +0100

    nvim: hide annoying fold column

--- nvim/lua/plugins/fold.lua
+++ nvim/lua/plugins/fold.lua
@@ -2,8 +2,8 @@ return {
   'kevinhwang91/nvim-ufo',
   dependencies = 'kevinhwang91/promise-async',
   config = function()
-    vim.o.foldcolumn = '1' -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldcolumn = '0' -- '1' is bad
+    vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
 
