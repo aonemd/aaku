4d7605b7e5b1f51c02cd14f11a7318b602c89d9f
aonemd <asalehmo7@gmail.com>
2025-12-08 11:22:54 +0100

    nvim: Add swift sourcekit lsp support

---
+++ nvim/after/lsp/sourcekit.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { 'xcrun', 'sourcekit-lsp' },
+  filetypes = { 'swift', 'objective-c', 'objective-cpp' },
+  root_markers = { 'Package.swift', '.git', '*.xcodeproj', '*.xcworkspace' },
+}
